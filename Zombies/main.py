c7fab59007e9981a8fc517237390d6699cdf462e
Adam Porembinski <75131243+plextor15@users.noreply.github.com>
2022-03-30 21:07:04 +0200

    Update main.py

--- Zombies/main.py
+++ Zombies/main.py
@@ -2,6 +2,9 @@ import pygame
 from sys import exit
 import getpass
 
+from pygame import mouse
+from pygame import key
+
 if __name__ == '__main__':
     pygame.init()
 
@@ -14,19 +17,25 @@ if __name__ == '__main__':
     #DEBUG
     test_surface = pygame.Surface((200,200))
     test_surface.fill('blue')
+    #
 
     #Background
     background = pygame.image.load('graphic/background/BG.png').convert_alpha()
 
     #Ground
-#    ground01 = pygame.image.load('graphic/ground/1.png')
-#    ground02 = pygame.image.load('graphic/ground/2.png')
-#    ground03 = pygame.image.load('graphic/ground/3.png')
+
     ground = pygame.image.load('graphic/ground/ground.png').convert_alpha()
     
     #Teksty
     game_font = pygame.font.Font('graphic/font/my_font.ttf', 100) #default - (None, 60)
-    text_surface = game_font.render(f"{getpass.getuser()} Witaj!", True, 'Red')
+    
+    #Welcome
+    text = game_font.render(f"{getpass.getuser()} Witaj!", True, 'Black')
+    text_rect = text.get_rect(topleft=(300,30))
+
+    #Score
+    score_text = game_font.render("My score:", False, 'Black')
+    score_text_rect = score_text.get_rect(topleft=(330,100))
 
     #PCs
     #Player
@@ -39,12 +48,39 @@ if __name__ == '__main__':
     #zombie_x_pos = 700
     zombie_rect = player.get_rect(topleft = (700, 430))
 
+
+    player_gravity = 0  #trzeba poprawic grawitacje
+
     #Main Game Loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()      #zeby byl exit code 0
+            
+            if event.type == pygame.MOUSEMOTION:
+                print( event.pos )
+                if player_rect.collidepoint( event.pos ):
+                    print("collide")
+            
+            if event.type == pygame.MOUSEBUTTONDOWN and player_rect.y == 430: # and player_gravity == 0:
+                #print( "Down" )
+                player_gravity = -20    #jump
+            if event.type == pygame.MOUSEBUTTONUP:
+                #print( "Up" )
+                pass    #zeby zblowalo double jump
+
+
+            if event.type == pygame.KEYDOWN:
+                #print("Key Down")
+                if event.key == pygame.K_SPACE:
+                    #print("Space")
+                    player_gravity = -20
+                if event.key == pygame.K_PAGEUP:
+                    print("PageUp")
+            if event.type == pygame.KEYUP:
+                print("Key Up")
+
 
         #chodzenie zombiaka w lewo
         zombie_rect.x -= 2
@@ -54,10 +90,45 @@ if __name__ == '__main__':
         #screen.blit(test_surface, (10,10))
         screen.blit(background, (0,0))
         screen.blit(ground, (0,540))
-        screen.blit(text_surface, (300,100))
+
+
+        pygame.draw.rect( screen, '#c0e8ec', text_rect, 0, 10 )
+        screen.blit(text, text_rect)
+        pygame.draw.rect( screen, '#c0e8ec', score_text_rect, 0, 10 )
+        screen.blit(score_text, score_text_rect)
+
 
         screen.blit(player, player_rect)
         screen.blit(zombie, zombie_rect)
 
-        pygame.display.update()
+
+
+
+        #Kolizje
+
+        #DEBUG ONLY
+        #print( player_rect.colliderect( zombie_rect ) )
+        #print( pygame.mouse.get_pos() )
+        
+        #mouse_pos = pygame.mouse.get_pos()
+        #if player_rect.collidepoint( mouse_pos ):
+            #print("LOLXD")
+            #print( pygame.mouse.get_pressed() )
+        
+        keys = pygame.key.get_pressed()
+        if keys[ pygame.K_SPACE ]:
+            print( "Jump" )
+        if keys[ pygame.K_F1 ]:
+            print( "F1" )
+        
+
+        pygame.display.update() ############# UPDATE #############
+
+
+        player_gravity += 1 #kiedys sie przepelni bufor
+        player_rect.y += player_gravity
+
+        if player_rect.y >= 430: player_rect.y = 430
+
+
         clock.tick(50)      #FPSy
